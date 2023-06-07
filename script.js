f0f665a7169abbb2b7ebf188396aaf3bf71b2375
Freittas1 <127066932+Freittas1@users.noreply.github.com>
2023-06-06 22:17:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    let displayValue = document.getElementById('display').value;
+    let result = eval(displayValue);
+    document.getElementById('display').value = result;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
